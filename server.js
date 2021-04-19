63bb355a968e67f23a29c0d49c45b0c982004f20
shadsm1125 <shadburn.sean@gmail.com>
2021-04-19 13:44:01 -0400

    Create index.html.. Add basic form. Add body parser. Add basic routing.

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
 
-console.log("May Node be with you");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/quotes", (req, res) => {
+  console.log(req.body);
+});
